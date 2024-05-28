2afbdd423fe39dee92f5a9870e0a4353e1cdf2e6
Daniel Donahay <Daniel.Donahay@analog.com>
2024-05-28 16:20:00 -0700

    Custome action is working aplication not started

--- CustomAction/CustomAction.cs
+++ CustomAction/CustomAction.cs
@@ -12,21 +12,21 @@ namespace CustomAction
         public static ActionResult LaunchApplication(Session session)
         {
             session.Log("Begin Launch Application");
+            Debugger.Launch();
 
             try
             {
-                string installDir = session["INSTALLFOLDER"];
+                string installDir = session.CustomActionData["INSTALLFOLDER"];
                 string exePath = System.IO.Path.Combine(installDir, "VNCOverlay.exe");
 
                 Process.Start(exePath);
+                return ActionResult.Success;
             }
             catch (Exception ex)
             {
                 session.Log("Error in LaunchApplication: " + ex.Message);
                 return ActionResult.Failure;
-            }
-
-            return ActionResult.Success;
+            } 
         }
     }
 }
